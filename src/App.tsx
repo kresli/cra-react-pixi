272473ca4035355b03d595f076b2afa2c0f9b3f4
Eduard Jacko <jacko.eduard@gmail.com>
2019-03-26 09:56:56 +0000

    added pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as PIXI from "pixi.js";
+import { Stage, Sprite } from "@inlet/react-pixi";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Stage>
+          <Sprite image={logo} x={100} y={100} />
+        </Stage>
       </div>
     );
   }
